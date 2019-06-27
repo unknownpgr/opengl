079f362a633f1472ca218b045af4feb6d3561cf3
UnknownPgr <unknownpgr@gmail.com>
2019-06-28 03:25:11 +0900

    append glm library

--- OpenGL/OpenGL/main.cpp
+++ OpenGL/OpenGL/main.cpp
@@ -4,6 +4,9 @@
 #include <fstream>
 #include <string>
 #include "stb_image.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 using namespace std;
 
